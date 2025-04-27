bde9e91086a765a2f8f9d47f6f9eaa2861aa5ef9
Avinash Kumar sharma <kumaravinashsharma82@gmail.com>
2025-04-27 21:55:52 +0530

    added event brdige trigger option

--- lambda.tf
+++ lambda.tf
@@ -24,6 +24,27 @@ resource "aws_iam_role" "iam_for_lambda_tf" {
     assume_role_policy=data.local_file.custom_policy.content
 }
 
+resource "aws_cloudwatch_event_rule" "lambda_trigger_rule" {
+  name                = "${var.function_name}_lambda-trigger-rule"
+  description         = "Triggers Lambda every 5 minutes"
+  schedule_expression = "cron(0/5 * * * ? *)"
+  event_bus_name      = "default"  # or your custom event bus name
+}
+
+resource "aws_cloudwatch_event_target" "lambda_target" {
+  rule      = aws_cloudwatch_event_rule.lambda_trigger_rule.name
+  target_id = "TargetLambda"
+  arn       = aws_lambda_function.test_lambda.arn
+}
+
+resource "aws_lambda_permission" "allow_eventbridge" {
+  statement_id  = "AllowExecutionFromEventBridge"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.test_lambda.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.lambda_trigger_rule.arn
+}
+
 output "lambda_function_details" {
   value = aws_lambda_function.test_lambda.arn
 }
--- lambda_function_payload/lambda.py
+++
@@ -1,8 +0,0 @@
-import json
-
-def lambda_handler(event,context):
-    print("demo testing")
-    return{
-        'statusCode': 200,
-        'body' : json.dumps('Hello world from lambda')
-    }
